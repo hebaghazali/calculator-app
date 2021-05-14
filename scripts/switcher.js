261c527dcd565f163458c95ac265ff0506109ab9
Heba Ghazaly <hebbaghazaly@gmail.com>
2021-05-14 21:36:13 +0200

    Initial commit

---
+++ scripts/switcher.js
@@ -0,0 +1,22 @@
+const body = document.getElementsByTagName('body')[0];
+const switcher = document.getElementById('switcher');
+
+const switcherPositions = switcher.getBoundingClientRect();
+
+const switcherOneThird = switcherPositions.width / 3; // 1/3 of the switcher width
+const firstThemePosition = switcherPositions.left + switcherOneThird;
+const secondThemePosition = firstThemePosition + switcherOneThird;
+const thirdThemePosition = switcherPositions.right;
+
+switcher.addEventListener('click', e => {
+  if (e.clientX <= firstThemePosition) {
+    switcher.children[0].style.transform = 'translateX(0)';
+    document.documentElement.setAttribute('data-theme', 'primary');
+  } else if (e.clientX <= secondThemePosition) {
+    switcher.children[0].style.transform = 'translateX(21.5px)';
+    document.documentElement.setAttribute('data-theme', 'secondary');
+  } else if (e.clientX <= thirdThemePosition) {
+    switcher.children[0].style.transform = 'translateX(44.5px)';
+    document.documentElement.setAttribute('data-theme', 'ternary');
+  }
+});
